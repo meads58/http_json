1ba95fba5cbada73fa08d5254c32ef5167d8423e
meads58 <meads109@gmail.com>
2015-07-08 09:49:51 +0100

    issue with safari and using the onclick event, the default behaviour was being fired and a 'cannot POST/' error would appear. Solution was to disable the default behaviour with event.preventDefault() after the onclick call. Server is getting posts and sessions have been implemented

--- server.js
+++ server.js
@@ -3,12 +3,30 @@ var app = express();
 var server = require('http').createServer(app);
 var path = require('path')
 
+var session = require('express-session')
+
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUnitialized: true
+}))
+
+app.use(function(request, response, next) {
+  var sess = request.session;
+  next();
+});
+
 app.get('/', function(request, response){
   response.sendFile(path.join(__dirname +'/index.html'));
 });
 
-app.post('/submitJson', function(request, response){
+app.post('/submitJson', jsonParser, function(request, response){
   console.log(request.body)
+  console.log(request.sessionID)
+  response.end();
 })
 
 server.listen(3000, function(){

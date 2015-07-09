260093d68d99b6fdc8ae718af8d04d3fe3d3c3d7
meads58 <meads109@gmail.com>
2015-07-09 12:51:49 +0100

    changes to only save file after submit is pressed. Caching of messages has been set up and the file is only saved after submit but the formatting of the json object isn't correct. The copy and paste messages need to be grouped as an array of objects within the parent object. Looking into this solution

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ var app = express();
 var fs = require('fs');
 var server = require('http').createServer(app);
 var path = require('path')
+var NodeCache = require( "node-cache" );
+var myCache = new NodeCache();
 
-var session = require('express-session')
+var session = require('express-session');
 
-var bodyParser = require('body-parser')
-var jsonParser = bodyParser.json()
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+var currentSessionID  = '';
+var count = 0
 
 app.use(session({
   secret: 'keyboard cat',
@@ -24,16 +28,57 @@ app.get('/', function(request, response){
   response.sendFile(path.join(__dirname +'/views/index.html'));
 });
 
-app.post('/submitJson', jsonParser, function(request, response){
-  var filePath = (path.join(__dirname + '/data/json_messages.txt'))
+app.post('/submitJson/:eventName', jsonParser, function(request, response){
+  currentSessionID = request.sessionID
   var message = request.body
-  message["sessionID"] = request.sessionID
+  var eventName = request.params.eventName
+  console.log(eventName)
+  eventAction(eventName, message)
+  //addToCache(message)
+
+  response.end();
+})
+
+var eventAction = function(eventName, message) {
+  switch(eventName) {
+    case 'copyAndPaste':
+      addToCache('copyAndPaste' + count, message);
+      count += 1
+      break;
+    case 'formCompletionTime':
+      writeToFile('formCompletionTime', message)
+    default:
+       addToCache(eventName, message);
+  }
+}
+
+var writeToFile = function(eventName, message) {
+  var filePath = (path.join(__dirname + '/data/json_messages.txt'))
+  var message = getAllMessages();
+  message["sessionID"] = currentSessionID
   fs.appendFile(filePath, JSON.stringify(message), function(err) {
     if(err) throw err;
     console.log(message)
   })
-  response.end();
-})
+}
+
+var getAllMessages = function(){
+  var messages = myCache.mget(myCache.keys());
+  return messages
+}
+
+var addToCache = function(cacheKey, message) {
+
+  myCache.set(cacheKey,message, function(err, success){
+    if( !err && success ){
+      console.log( success );
+    }
+  })
+  // count = count + 1
+  // console.log(myCache.keys())
+  // console.log(myCache.mget(myCache.keys()));
+}
+
 
 server.listen(3000, function(){
   console.log("Server listening on port 3000");
